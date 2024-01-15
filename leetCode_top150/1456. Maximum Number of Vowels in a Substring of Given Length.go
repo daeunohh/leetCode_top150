e134eaf81b6b925ef4aa91cadf7ae23a2b468960
daeun <5ekdmsdldl@gmail.com>
2024-01-15 23:39:01 +0900

    Maximum Number of Vowels in a Substring of Given Length (8 ms)

--- leetCode_top150/1456. Maximum Number of Vowels in a Substring of Given Length.go
+++ leetCode_top150/1456. Maximum Number of Vowels in a Substring of Given Length.go
@@ -1,12 +1,5 @@
-func isVowel(s string) bool{
-	vowels := []string{"a", "e", "i", "o", "u"}
-  
-	for _, letter := range vowels{
-	  if(s == letter){
-		return true
-	  }
-	}
-	return false
+func isVowel(s byte) bool{  
+	return s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u'
   }
   
   func maxVowels(s string, k int) int {
@@ -14,28 +7,25 @@ func isVowel(s string) bool{
 	maxCnt := 0
 	
 	for i := 0; i < k; i++{
-	  if(isVowel(string(s[i]))){
+	  if(isVowel(s[i])){
 		vCnt += 1
 	  }
 	}
 	maxCnt = vCnt
-  
+	
 	front := k
-	back := 0
 	for front < len(s){
-	  if(isVowel(string(s[front]))){
+	  if(isVowel((s[front]))){
 		vCnt += 1
 		}
-	  if(isVowel(string(s[back]))){
+	  if(isVowel((s[front - k]))){
 		vCnt -= 1
 	  }
 	  if vCnt > maxCnt {
 		maxCnt = vCnt
 	  }
-	  
 	  front += 1
-	  back += 1
-	  
 	}
+
 	return maxCnt
   }
