6469b013196d1804e94915504a214c732da25a47
daeun <5ekdmsdldl@gmail.com>
2024-01-15 23:36:16 +0900

    199. Binary Tree Right Side View - 2 (3ms)

--- leetCode_top150/199. Binary Tree Right Side View.cpp
+++ leetCode_top150/199. Binary Tree Right Side View.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    vector<int> maxRight;
     vector<int> vals;
 
-    void func(TreeNode* root, int depth, int rightness) {
-        if (depth >= maxRight.size()) {
-            maxRight.push_back(rightness);
+    void func(TreeNode* root, int depth) {
+        if (depth >= vals.size()) {
             vals.push_back(root->val);
         }
 
-        if (root->right != nullptr) func(root->right, depth + 1, rightness + 1);
-        if (root->left != nullptr)  func(root->left, depth + 1, rightness - 1);
+        if (root->right != nullptr) func(root->right, depth + 1);
+        if (root->left != nullptr)  func(root->left, depth + 1);
     }
 
     vector<int> rightSideView(TreeNode* root) {
         if (root == nullptr) return {};
-        func(root, 0, 0);
+        func(root, 0);
         return vals;
     }
 };
