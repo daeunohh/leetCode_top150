ce006fc43e59fe78a583f6645173789594a917fa
daeun <5ekdmsdldl@gmail.com>
2024-01-17 22:53:01 +0900

    push without abs

--- leetCode_top150/530. Minimum Absolute Difference in BST.cpp
+++ leetCode_top150/530. Minimum Absolute Difference in BST.cpp
@@ -18,11 +18,17 @@ public:
 
             if (curP.node->left != nullptr) {
                 int leftGap = curP.node->val - curP.node->left->val;
-                q.push({ curP.node->left, min(leftGap, leftGap + curP.minGap) });
+                if (abs(leftGap) < abs(leftGap + curP.minGap))
+                    q.push({ curP.node->left, leftGap });
+                else
+                    q.push({ curP.node->left, leftGap + curP.minGap });
             }
             if (curP.node->right != nullptr) {
                 int rightGap = curP.node->right->val - curP.node->val;
-                q.push({ curP.node->right, min(rightGap, rightGap + curP.minGap) });
+                if (abs(rightGap) < abs(rightGap + curP.minGap))
+                    q.push({ curP.node->right, rightGap });
+                else
+                    q.push({ curP.node->right, rightGap + curP.minGap });
             }
 
             if (curP.node->left == nullptr && curP.node->right == nullptr) {
