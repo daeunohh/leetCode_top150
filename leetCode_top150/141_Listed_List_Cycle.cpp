ad5f79c0ce7c9cea79d6127f5e4cfa0eac6bd96e
daeun <5ekdmsdldl@gmail.com>
2024-02-27 23:30:53 +0900

    148. Sort List

--- leetCode_top150/141_Listed_List_Cycle.cpp
+++ leetCode_top150/141_Listed_List_Cycle.cpp
@@ -1,30 +1,74 @@
 #include <cstddef>
+#include <vector>
+#include <iostream>
+using namespace std;
 
 struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(0) {}
   };
- 
+
+////////////////////////////////////////////////////////////
+
 class Solution {
 public:
-    bool hasCycle(ListNode* head) {
-        if (head == NULL) return false;
+    ListNode* mergeSort(ListNode* a, ListNode* b) {
+        ListNode* ret = new ListNode(0); 
+        ListNode* retHead(ret);
 
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-
-        while (fast != slow) {
-            if (fast == NULL) return false;
-            else if (fast->next == NULL) return false;
-
-            slow = slow->next;
-            fast = fast->next->next;
+        while (a != nullptr && b != nullptr) {
+            if (a->val < b->val) {
+                ret->next = a;
+                a = a->next;
+                ret = ret->next;
+            }
+            else {
+                ret->next = b;
+                b = b->next;
+                ret = ret->next;
+            }
+        }
+        if (a != nullptr) {
+            ret->next = a;
+        }
+        if(b != nullptr) {
+            ret->next = b;
         }
 
-        return true;
+        return retHead->next;
+    }
+
+    ListNode* sortList(ListNode* head) {
+        vector<ListNode*> nodeList;
+        auto p = head;
+        nodeList.push_back(p);
+
+        while (p != nullptr) {
+            if (p->next == nullptr) {
+                break;
+            }
+
+            if (p->next->val < p->val) {
+                auto pre = p;
+                p = p->next;
+                pre->next = nullptr;
+                nodeList.push_back(p);
+            }
+            else {
+                p = p->next;
+            }
+        }
+
+        auto res = nodeList[0];
+        for (int i = 1; i < nodeList.size(); i++) {
+            res = mergeSort(res, nodeList[i]);
+        }
+
+        return res;
     }
 };
+
 
 
 
