863bff83e3bce052ed59354db84629d8bfb6692f
daeun <5ekdmsdldl@gmail.com>
2024-03-05 23:44:36 +0900

    53. Maximum Subarray

--- leetCode_top150/main.cpp
+++ leetCode_top150/main.cpp
@@ -2,40 +2,22 @@
 
 class Solution {
 public:
-    int totalSz = 0;
-    vector<vector<int>>* grid;
+    int maxSubArray(vector<int>& nums) {
+		int totalMax = nums[0];
+		int prevMax = totalMax;
 
-    bool isLeaf(int x, int y, int sz) {
-        int defaultValue = (*grid)[x][y];
-        for (int i = x; i < x + sz; i++) {
-            for (int j = y; j < y + sz; j++) {
-                if ((*grid)[i][j] != defaultValue) return false;
-            }
-        }
-        return true;
-    }
+		for (int i = 1; i < nums.size(); i++) {
+			if (nums[i] > prevMax + nums[i]) {
+				prevMax = nums[i];
+			}
+			else {
+				prevMax = prevMax + nums[i];
+			}
 
-    Node* check(int x, int y, int sz) {
-        Node* head = new Node();
-        head->isLeaf = isLeaf(x, y, sz);
-        // std::cout << head->isLeaf << std::endl;
-        if (head->isLeaf) {
-            head->val = (*grid)[x][y];
-            return head;
-        }
+			if (prevMax > totalMax) totalMax = prevMax;
+		}
 
-        head->topLeft = check(x, y, sz / 2);
-        head->bottomLeft = check(x + sz / 2, y, sz / 2);
-        head->topRight = check(x, y + sz / 2, sz / 2);
-        head->bottomRight = check(x + sz / 2, y + sz / 2, sz / 2);
-
-        return head;
-    }
-
-    Node* construct(vector<vector<int>>& grid) {
-        this->grid = &grid;
-        totalSz = grid.size();
-        return check(0, 0, totalSz);
+		return totalMax;
     }
 };
 
