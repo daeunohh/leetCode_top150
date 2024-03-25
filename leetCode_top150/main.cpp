ee2dfc3f1ff9743ef2295caf8e833c35dda7424e
daeun <5ekdmsdldl@gmail.com>
2024-03-25 23:12:11 +0900

    46. Permutations

--- leetCode_top150/main.cpp
+++ leetCode_top150/main.cpp
@@ -1,49 +1,29 @@
 #include "main.h"
-#include <queue>
 
 class Solution {
 public:
-    queue<string> q;
-    vector<vector<string>> itoc;
+    vector<vector<int>> ret;
+    vector<bool> included;
+    vector<int> current;
 
-    Solution() {
-        itoc.assign(10, {});
-        itoc[2] = { "a", "b", "c" };
-        itoc[3] = { "d", "e", "f" };
-        itoc[4] = { "g", "h", "i" };
-        itoc[5] = { "j", "k", "l" };
-        itoc[6] = { "m", "n", "o" };
-        itoc[7] = { "p", "q", "r", "s" };
-        itoc[8] = { "t", "u", "v" };
-        itoc[9] = { "w","x", "y", "z" };
+    void func(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (!included[i]) {
+                current.push_back(nums[i]); included[i] = true;
+                func(nums);
+                current.pop_back(); included[i] = false;
+            }
+        }
+        if(current.size() == nums.size())
+            ret.push_back(current);
+        return;
     }
-
-    vector<string> letterCombinations(string digits) {
-        int digitLen = digits.length();
-        if (digits.empty()) return {};
-        vector<string> ret;
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        included.assign(nums.size(), false);
         
-        for (string c : itoc[digits[0] - '0']) {
-            q.push((c));
-        }
-
-        while (!q.empty()) {
-            string str = q.front(); q.pop();
-            if (str.length() == digitLen) {
-                ret.push_back(str);
-            }
-            else {
-                for (string c : itoc[digits[str.length()] - '0']) {
-                    q.push(str + c);
-                }
-            }
-        }
+        func(nums);
 
         return ret;
     }
 };
-
-void main() {
-    Solution s;
-    s.letterCombinations("");
-}
