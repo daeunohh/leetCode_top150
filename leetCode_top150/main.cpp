5c684340b548561b015cd40fc7f449576a6a699c
daeun <5ekdmsdldl@gmail.com>
2024-04-07 18:32:16 +0900

    use binary search for finding start and end of the number - but still too slow

--- leetCode_top150/main.cpp
+++ leetCode_top150/main.cpp
@@ -23,10 +23,38 @@ public:
         }
 
         if (nums[mid] == target) {
-            start = mid - 1; end = mid + 1;
+            /*start = mid - 1; end = mid + 1;
             while (start >= 0 && nums[start] == target) start--;
             while (end < nums.size() && nums[end] == target) end++;
-            return { start + 1, end - 1 };
+            return { start + 1, end - 1 };*/
+
+            // search frontward part
+            int fstart = start; int fend = mid;
+            int fmid = (fend + fstart) / 2;
+            while(fstart <= fend){
+                fmid = (fend + fstart) / 2;
+                if (nums[fmid] == target) {
+                    fend = fmid - 1;
+                }
+                else {
+                    fstart = fmid + 1;
+                }
+            }
+
+            // search backward part
+            int bstart = mid; int bend = end;
+            int bmid = (bend + bstart) / 2;
+            while (bstart <= bend) {
+                bmid = (bend + bstart) / 2;
+                if (nums[bmid] == target) {
+                    bstart = bmid + 1;
+                }
+                else {
+                    bend = bmid - 1;
+                }
+            }
+            
+            return {fstart, bend};
         }
 
         return { -1, -1 };
