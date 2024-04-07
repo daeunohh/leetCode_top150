b208ab6d74f8f4bfc3c841219e2ad1e117b36413
daeun <5ekdmsdldl@gmail.com>
2024-04-07 18:50:10 +0900

    little faster

--- leetCode_top150/main.cpp
+++ leetCode_top150/main.cpp
@@ -11,7 +11,33 @@ public:
         while (start <= end) {
             mid = (end + start) / 2;
             if (nums[mid] == target) {
-                break;
+                // search frontward part
+                int fstart = start; int fend = mid;
+                int fmid = (fend + fstart) / 2;
+                while (fstart <= fend) {
+                    fmid = (fend + fstart) / 2;
+                    if (nums[fmid] == target) {
+                        fend = fmid - 1;
+                    }
+                    else {
+                        fstart = fmid + 1;
+                    }
+                }
+
+                // search backward part
+                int bstart = mid; int bend = end;
+                int bmid = (bend + bstart) / 2;
+                while (bstart <= bend) {
+                    bmid = (bend + bstart) / 2;
+                    if (nums[bmid] == target) {
+                        bstart = bmid + 1;
+                    }
+                    else {
+                        bend = bmid - 1;
+                    }
+                }
+
+                return { fstart, bend };
             }
 
             if (nums[mid] > target) {
@@ -20,41 +46,6 @@ public:
             else {
                 start = mid + 1;
             }
-        }
-
-        if (nums[mid] == target) {
-            /*start = mid - 1; end = mid + 1;
-            while (start >= 0 && nums[start] == target) start--;
-            while (end < nums.size() && nums[end] == target) end++;
-            return { start + 1, end - 1 };*/
-
-            // search frontward part
-            int fstart = start; int fend = mid;
-            int fmid = (fend + fstart) / 2;
-            while(fstart <= fend){
-                fmid = (fend + fstart) / 2;
-                if (nums[fmid] == target) {
-                    fend = fmid - 1;
-                }
-                else {
-                    fstart = fmid + 1;
-                }
-            }
-
-            // search backward part
-            int bstart = mid; int bend = end;
-            int bmid = (bend + bstart) / 2;
-            while (bstart <= bend) {
-                bmid = (bend + bstart) / 2;
-                if (nums[bmid] == target) {
-                    bstart = bmid + 1;
-                }
-                else {
-                    bend = bmid - 1;
-                }
-            }
-            
-            return {fstart, bend};
         }
 
         return { -1, -1 };
