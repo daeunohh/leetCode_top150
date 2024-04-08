4c884cd0661173617c17a6ffc70afdf9accaaad9
daeun <5ekdmsdldl@gmail.com>
2024-04-08 19:53:07 +0900

    tmp commit

--- leetCode_top150/main.cpp
+++ leetCode_top150/main.cpp
@@ -7,47 +7,33 @@ public:
 
         int start = 0; int end = nums.size() - 1;
         int mid = 0;
+        int retStart = -1; int retEnd = -1;
 
         while (start <= end) {
+            if (nums[start] == target) {
+                retStart = start;
+            }
+            if (nums[end] == target) {
+                retEnd == end;
+            }
+            if (retStart != -1 && retEnd != -1) return { retStart, retEnd };
+
             mid = (end + start) / 2;
             if (nums[mid] == target) {
-                // search frontward part
-                int fstart = start; int fend = mid;
-                int fmid = (fend + fstart) / 2;
-                while (fstart <= fend) {
-                    fmid = (fend + fstart) / 2;
-                    if (nums[fmid] == target) {
-                        fend = fmid - 1;
-                    }
-                    else {
-                        fstart = fmid + 1;
-                    }
-                }
 
-                // search backward part
-                int bstart = mid; int bend = end;
-                int bmid = (bend + bstart) / 2;
-                while (bstart <= bend) {
-                    bmid = (bend + bstart) / 2;
-                    if (nums[bmid] == target) {
-                        bstart = bmid + 1;
-                    }
-                    else {
-                        bend = bmid - 1;
-                    }
-                }
-
-                return { fstart, bend };
-            }
-
-            if (nums[mid] > target) {
-                end = mid - 1;
             }
             else {
-                start = mid + 1;
+
             }
+
+
+
+
         }
 
         return { -1, -1 };
     }
 };
+
+
+
