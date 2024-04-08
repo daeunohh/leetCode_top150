9f0748937549d1205ec1ecb6c037204378c4db6c
daeun <5ekdmsdldl@gmail.com>
2024-04-08 20:27:05 +0900

    find first and last element with two different bin searching

--- leetCode_top150/34. Find First and Last Position of Element in Sorted Array.cpp
+++ leetCode_top150/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -1,33 +1,52 @@
-#include "main.h"
+
 
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         if (nums.size() == 0) return { -1, -1 };
 
-        int start = 0; int end = nums.size() - 1;
-        int mid = 0;
+        int retStart = -1; int retEnd = -1;
+        {
+            int start = 0; int end = nums.size() - 1;
+            int mid = 0;
 
-        while (start <= end) {
-            mid = (end + start) / 2;
-            if (nums[mid] == target) {
-                break;
+            while (start <= end) {
+                mid = (end + start) / 2;
+                if (nums[mid] == target) {
+                    if (mid == 0 || nums[mid - 1] != target) {
+                        retStart = mid; break;
+                    }
+                }
+                if (nums[mid] >= target) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
             }
+        }
+        {
+            int start = 0; int end = nums.size() - 1;
+            int mid = 0;
 
-            if (nums[mid] > target) {
-                end = mid - 1;
-            }
-            else {
-                start = mid + 1;
+            while (start <= end) {
+                mid = (end + start) / 2;
+                if (nums[mid] == target) {
+                    if (mid == nums.size() - 1 || nums[mid + 1] != target) {
+                        retEnd = mid; break;
+                    }
+                }
+                if (nums[mid] > target) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
             }
         }
 
-        if (nums[mid] == target) {
-            start = mid - 1; end = mid + 1;
-            while (start >= 0 && nums[start] == target) start--;
-            while (end < nums.size() && nums[end] == target) end++;
-            return { start + 1, end - 1 };
-        }
+        if (retStart != -1 && retEnd != -1) return { retStart, retEnd };
+
 
         return { -1, -1 };
     }
