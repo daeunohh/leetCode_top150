b2148278405ab93f61bedd1cb736510986ba2517
daeun <5ekdmsdldl@gmail.com>
2024-05-12 19:36:00 +0900

    use BST (binary search tree) to get the kth biggest element

--- leetCode_top150/main.cpp
+++ leetCode_top150/main.cpp
@@ -1,39 +1,83 @@
-#include "main.h"
+// #include "main.h"
+const int MIN = -1001;
 
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        if (nums.size() == 0) return { -1, -1 };
+    struct Node {
+        Node* left= nullptr;
+        Node* right = nullptr;
+        int val = MIN; int cnt = 0;
 
-        int start = 0; int end = nums.size() - 1;
-        int mid = 0;
-        int retStart = -1; int retEnd = -1;
+        Node(int _val) : left(nullptr), right(nullptr), val(_val), cnt(1) {}
+    };
 
-        while (start <= end) {
-            if (nums[start] == target) {
-                retStart = start;
-            }
-            if (nums[end] == target) {
-                retEnd == end;
-            }
-            if (retStart != -1 && retEnd != -1) return { retStart, retEnd };
+    int k = 0;
+    Node* head = nullptr;
 
-            mid = (end + start) / 2;
-            if (nums[mid] == target) {
-
-            }
-            else {
-
+    void makeTree(vector<int>& nums) {
+        for (auto n : nums) {
+            if (head == nullptr) {
+                head = new Node(n);
+                continue;
             }
 
+            Node* cur = head;
+            while (1) {
+                if (cur->val == n) {
+                    cur->cnt++;
+                    break;
+                }
 
+                if (cur->val > n) {
+                    if (cur->left == nullptr) {
+                        cur->left = new Node(n);
+                        break;
+                    } else {
+                        cur = cur->left;
+                    }
+                } else {
+                    if (cur->right == nullptr) {
+                        cur->right = new Node(n);
+                        break;
+                    }
+                    else {
+                        cur = cur->right;
+                    }
+                }
+            }
+        }
+    }
 
-
+    int cnt = 0; int kthVal = MIN;
+    void searchKth(Node* cur) {
+        if (cur->right != nullptr) {
+            searchKth(cur->right);
         }
 
-        return { -1, -1 };
+        if (kthVal == MIN) {
+            cnt += cur->cnt;
+            printf("%d %d %d \n", cur->val, cur->cnt, cnt);
+            if (cnt >= k) {
+                kthVal = cur->val;
+                return;
+            }
+        }
+        else return;
+
+        if (kthVal == MIN && cur->left != nullptr) {
+            searchKth(cur->left);
+        }
+        else return;
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+        this->k = k;
+
+        makeTree(nums);
+        searchKth(head);
+
+        return kthVal;
     }
 };
-
 
 
