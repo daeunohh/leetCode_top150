65a5471898125da2b5619433940819bbf9bc7329
daeun <5ekdmsdldl@gmail.com>
2024-05-12 19:42:11 +0900

    optimial answer using min-heap

--- leetCode_top150/main.cpp
+++ leetCode_top150/main.cpp
@@ -1,83 +1,15 @@
-// #include "main.h"
-const int MIN = -1001;
-
 class Solution {
 public:
-    struct Node {
-        Node* left= nullptr;
-        Node* right = nullptr;
-        int val = MIN; int cnt = 0;
+    int findKthLargest(std::vector<int>& nums, int k) {
+        std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap(nums.begin(), nums.begin() + k);
 
-        Node(int _val) : left(nullptr), right(nullptr), val(_val), cnt(1) {}
-    };
-
-    int k = 0;
-    Node* head = nullptr;
-
-    void makeTree(vector<int>& nums) {
-        for (auto n : nums) {
-            if (head == nullptr) {
-                head = new Node(n);
-                continue;
-            }
-
-            Node* cur = head;
-            while (1) {
-                if (cur->val == n) {
-                    cur->cnt++;
-                    break;
-                }
-
-                if (cur->val > n) {
-                    if (cur->left == nullptr) {
-                        cur->left = new Node(n);
-                        break;
-                    } else {
-                        cur = cur->left;
-                    }
-                } else {
-                    if (cur->right == nullptr) {
-                        cur->right = new Node(n);
-                        break;
-                    }
-                    else {
-                        cur = cur->right;
-                    }
-                }
+        for (int i = k; i < nums.size(); i++) {
+            if (nums[i] > min_heap.top()) {
+                min_heap.pop();
+                min_heap.push(nums[i]);
             }
         }
-    }
 
-    int cnt = 0; int kthVal = MIN;
-    void searchKth(Node* cur) {
-        if (cur->right != nullptr) {
-            searchKth(cur->right);
-        }
-
-        if (kthVal == MIN) {
-            cnt += cur->cnt;
-            printf("%d %d %d \n", cur->val, cur->cnt, cnt);
-            if (cnt >= k) {
-                kthVal = cur->val;
-                return;
-            }
-        }
-        else return;
-
-        if (kthVal == MIN && cur->left != nullptr) {
-            searchKth(cur->left);
-        }
-        else return;
-    }
-
-    int findKthLargest(vector<int>& nums, int k) {
-        this->k = k;
-
-        makeTree(nums);
-        searchKth(head);
-
-        return kthVal;
+        return min_heap.top();
     }
 };
-
-
