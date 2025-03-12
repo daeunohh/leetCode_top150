2aec8d1e85069c231dd310bfba55480267c15650
Da-Eun Oh <76278018+daeunohh@users.noreply.github.com>
2025-03-12 06:28:44 +0000

    Still DRE

--- leetCode_top150/373. Find K Pairs with Smallest Sums.cpp
+++ leetCode_top150/373. Find K Pairs with Smallest Sums.cpp
@@ -4,34 +4,35 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        priority_queue<pair<int, pair<int, int>>> pq;
-        vector<vector<int>> ret;
+        vector<vector<int>> ret; 
+        priority_queue < pair<int, pair<int, int>>> pq; 
 
-        int idx = 0;
-        while (1) {
-            int idx1 = 0; int idx2 = 0;
-            if (idx < nums1.size()) idx1 = idx; 
-            else idx1 = nums1.size();
-            if (idx < nums2.size()) idx2 = idx; 
-            else idx2 = nums2.size();
+        int i1 = 0; int i2 = 0;
+        ret.push_back({ nums1[0], nums2[0] });
 
-            if (idx != 0) {
-                for (int i = 0; i < idx2; i++) {
-                    pq.push({ -(nums1[idx1] + nums2[i]), {idx1, i} });
+        while (ret.size() < k) {
+
+            if ((i1 == nums1.size() - 1) || (i2 == nums2.size() - 1)) {
+                if (i1 == nums1.size() - 1) {
+                    ret.push_back({ nums1[i1], nums2[i2 + 1] });
+                    i2++;
+                    continue;
                 }
-                for (int i = 0; i < idx1; i++) {
-                    pq.push({ -(nums1[i] + nums2[idx2]), {i, idx2} });
+                else { // (i2 == nums2.size() - 1)
+                    ret.push_back({ nums1[i1 + 1], nums2[i2] });
+                    i1++;
+                    continue;
                 }
-            }            
-            pq.push({ -(nums1[idx1] + nums2[idx2]), {idx1, idx2} });
-            idx++;
+            }
 
-            if (pq.size() >= k) break;
-        }
-
-        for (int i = 0; i < k; i++) {
-            pair<int, pair<int, int>> smallestPair = pq.top(); pq.pop();
-            ret.push_back({ nums1[smallestPair.second.first], nums2[smallestPair.second.second] });
+            if ((nums1[i1] + nums2[i2 + 1]) < (nums1[i1 + 1] + nums2[i2])) {
+                ret.push_back({ nums1[i1], nums2[i2 + 1] });
+                i2++;
+            }
+            else {
+                ret.push_back({ nums1[i1 + 1], nums2[i2]} );
+                i1++;
+            }
         }
 
         return ret;
