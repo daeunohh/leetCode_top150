ddbb0b8dd3caf7d5ddb8d9e6e51a9813f7613482
Da-Eun Oh <76278018+daeunohh@users.noreply.github.com>
2025-03-25 14:40:11 +0000

    35. Search Insert Position

--- leetCode_top150/main.cpp
+++ leetCode_top150/main.cpp
@@ -1,22 +1,6 @@
 class Solution {
-public:
-
-    class Heap{
-        struct Node{
-            int value;
-            Node* left= nullptr;
-            Node* right = nullptr;
-        };
-
-        Node* head = nullptr;
-
-        Heap(int val){
-            head = new Node();
-            head->value = val;
+    public:
+        int searchInsert(vector<int>& nums, int target) {
+            
         }
-    }
-
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        
-    }
-};
+    };
