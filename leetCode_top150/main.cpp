545ebc93422854961df6fb319aa6114a6c9d3225
Da-Eun Oh <76278018+daeunohh@users.noreply.github.com>
2025-03-25 15:17:37 +0000

    on O(logN)

--- leetCode_top150/main.cpp
+++ leetCode_top150/main.cpp
@@ -6,11 +6,28 @@ using namespace std;
 class Solution {
     public:
         int searchInsert(vector<int>& nums, int target) {
-            for(auto it = nums.begin(); it != nums.end(); it++){
-                if(*it >= target){
-                    return distance(nums.begin(), it);
+            auto front = nums.begin();
+            auto tail = nums.end() - 1;
+            auto idx = tail;
+
+            while(distance(front, tail) >= 0){
+                auto mid = front + (distance(front, tail) / 2);
+                // cout << distance(nums.begin(), front) << " " << distance(nums.begin(), tail) << " " << distance(nums.begin(), mid) << endl;
+                if(*mid == target) return distance(nums.begin(), mid);
+                if(*mid < target){
+                    front = mid + 1;
+                }
+                else{
+                    if(mid == nums.begin()){
+                        return 0;
+                    }
+                    tail = mid - 1;
+                    idx = mid;
                 }
             }
-            return nums.size();
+            if(idx == tail){
+                return nums.size();
+            }
+            return distance(nums.begin(), idx);
         }
     };
