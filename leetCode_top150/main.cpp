acb7c40d379ecc77b98c747fe0bec1df05b71269
Da-Eun Oh <76278018+daeunohh@users.noreply.github.com>
2025-03-25 14:51:36 +0000

    on O(N)

--- leetCode_top150/main.cpp
+++ leetCode_top150/main.cpp
@@ -1,6 +1,16 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
 class Solution {
     public:
         int searchInsert(vector<int>& nums, int target) {
-            
+            for(auto it = nums.begin(); it != nums.end(); it++){
+                if(*it >= target){
+                    return distance(nums.begin(), it);
+                }
+            }
+            return nums.size();
         }
     };
