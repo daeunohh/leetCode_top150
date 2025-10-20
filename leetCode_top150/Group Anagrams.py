37e63d6bd938e7e5b48db0f648d1ea55b2e47049
daeunoh <daeunoh98@gmail.com>
2025-10-19 22:58:49 -0400

    group anagram 2

--- leetCode_top150/Group Anagrams.py
+++ leetCode_top150/Group Anagrams.py
@@ -1,17 +1,11 @@
-# Time: M* NlogN, Space:N 
+# Time: M*N, Space:M*N  
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashmap = {}
-        for i in range(len(strs)):
-            sortedstr = str(sorted(strs[i]))
-            hashmap[sortedstr] = hashmap.get(sortedstr, []) + [i]
+        hashmap = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            hashmap[tuple(count)].append(s)
 
-        ret = []
-        for comb in hashmap:
-            idxs = hashmap[comb]
-            tmp = []
-            for idx in idxs:
-                tmp.append(strs[idx])
-            ret.append(tmp)
-
-        return ret
+        return list(hashmap.values())
